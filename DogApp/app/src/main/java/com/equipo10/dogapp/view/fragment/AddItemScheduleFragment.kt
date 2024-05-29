a24f45fd90e2e5743b256eb1bb3e144a57cc5bbf
juliantello <yilver.tello@correounivalle.edu.co>
2024-05-29 18:04:26 -0500

    vista final

--- DogApp/app/src/main/java/com/equipo10/dogapp/view/fragment/AddItemScheduleFragment.kt
+++ DogApp/app/src/main/java/com/equipo10/dogapp/view/fragment/AddItemScheduleFragment.kt
@@ -18,6 +18,7 @@ import com.equipo10.dogapp.databinding.FragmentAdminCitasBinding
 import com.equipo10.dogapp.model.Schedule
 import com.equipo10.dogapp.viewmodel.ScheduleViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import android.widget.Spinner;
 
 @AndroidEntryPoint
 class AddItemScheduleFragment : Fragment() {
@@ -44,10 +45,10 @@ class AddItemScheduleFragment : Fragment() {
     }
 
     private fun setupAutoCompleteTextView() {
-        val symptoms = resources.getStringArray(R.array.symptoms_array)
+        val symptoms = resources.getStringArray(R.array.arraySintomas)
         Log.d("Info", symptoms.toString())
-        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, symptoms)
-        binding.addSymptomsAutoComplete.setAdapter(adapter)
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, symptoms)
+        binding.addSintomas.setAdapter(adapter)
     }
 
     private fun controladores(){
@@ -66,18 +67,16 @@ class AddItemScheduleFragment : Fragment() {
             binding.addRaza,
             binding.addNameOwner,
             binding.addPhone,
-            binding.addSymptomsAutoComplete
+            binding.addSintomas
         )
 
         val textWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                binding.btnSaveAppointment.isEnabled = requiredFields.all { it.text.isNotEmpty() }
+                binding.btnSaveAppointment.isEnabled = requiredFields.all { it.toString().isNotEmpty() }
             }
             override fun afterTextChanged(s: Editable?) {}
         }
-
-        requiredFields.forEach { it.addTextChangedListener(textWatcher) }
     }
 
     private fun saveSchedule(){
@@ -85,7 +84,7 @@ class AddItemScheduleFragment : Fragment() {
         val race: String = binding.addRaza.text.toString()
         val name_owner: String = binding.addNameOwner.text.toString()
         val phone: String = binding.addPhone.text.toString()
-        val sysptoms: String = binding.addSymptomsAutoComplete.text.toString()
+        val sysptoms: String = binding.addSintomas.toString()
 
         val schedule = Schedule(name = name, race = race, name_owner = name_owner, phone = phone,  sysptoms = sysptoms)
         scheduleViewModel.saveSchedule(schedule)
